e58618ee34cfa681f64ba7582dbd301dc493f34d
wde11 <tperalta_230000001926@uic.edu.ph>
2025-07-17 22:08:10 +0800

    First flutter chat app.

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Flutter Chat App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(),
+        floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              print("Hi");
+            }),
+        body: Text("Hey"),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            print("Yo!");
+          }),
+      body: Text("Hello"),
+    );
+  }
+}
